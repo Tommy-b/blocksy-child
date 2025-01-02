35481514de94a39fd6524443f7104ac66cf12ded
Tommy-b <thomasbuzelli@gmail.com>
2025-01-02 02:37:57 -0700

    init repo

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+if (! defined('WP_DEBUG')) {
+	die( 'Direct access forbidden.' );
+}
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style('blocksy-child-style', get_stylesheet_uri());
+});
+
+function load_custom_wp_admin_style(){
+	wp_register_style( 'custom_wp_admin_css', get_bloginfo('stylesheet_directory') . '/style.css', false, '1.0.0' );
+	wp_enqueue_style( 'custom_wp_admin_css' );
+}
+add_action('admin_enqueue_scripts', 'load_custom_wp_admin_style');
+
+add_filter( 'gform_ppcp_disable_funding', function( $disabled_funding ) {
+    $disabled_funding[] = 'credit'; // PayPal Credit (US, UK).
+    $disabled_funding[] = 'paylater'; // Pay Later (US, UK), Pay in 4 (AU), 4X PayPal (France), Später Bezahlen (Germany).
+    return $disabled_funding;
+} );
